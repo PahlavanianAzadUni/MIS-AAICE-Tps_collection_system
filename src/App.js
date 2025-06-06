e7bd165613f5a0610d600a4d9b80c4a7442245c7
Mohammad <pahlavanianmohammad@gmail.com>
2025-06-06 23:09:57 +0330

    Add application footer

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-// Import the component you created
 import TPSForm from './components/TPSForm';
 import TransactionList from './components/TransactionList';
 import Report from './components/Report';
+import Footer from './components/Footer'; // 1. Import the Footer
 import './App.css';
 
 function App() {
     return (
         <Router>
-            <div className="App">
+            <div className="App-container"> {/* A wrapper to help position the footer */}
                 <header className="App-header">
-                    <h1>TPS Web System of AAICE</h1>
+                    <h1>TPS Web System - AI & CE Student Association</h1>
                 </header>
-                <main>
+                <main className="App-content">
                     <Routes>
                         <Route path="/" element={<Dashboard />} />
-                        {/* Enable the route to the form */}
                         <Route path="/form/:category" element={<TPSForm />} />
                         <Route path="/transactions" element={<TransactionList />} />
                         <Route path="/report" element={<Report />} />
                     </Routes>
                 </main>
+                <Footer /> {/* 2. Add the Footer component here */}
             </div>
         </Router>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="app-footer">
+            <p>Created by Pahlavanian Mohammad in 2025</p>
+            <p>For the course of MIS in CE of IAU </p>
+            <p>Term 4032</p>
+        </footer>
+    );
+}
+
+export default Footer;
